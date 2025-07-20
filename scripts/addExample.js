45c57c20bd245e027beb74de550fddc76a1442b7
eunchae <jjeoneunchae@gmail.com>
2025-07-20 18:31:50 +0900

    update add example force

--- scripts/addExample.js
+++ scripts/addExample.js
@@ -56,39 +56,63 @@ function createImageEmbed({ imgUrl, alt = "", width = 600, target = null, margin
   return container;
 }
 
-createImageEmbed({
-  imgUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWMxR0yIjV7awYjGgYKnc1En39uJQkjpzmKg&s",
-  target: document.querySelector('.chapter-opening-quote'),
-
-});
-createImageEmbed({
-  imgUrl: "https://mblogthumb-phinf.pstatic.net/MjAyMDA1MTVfNjkg/MDAxNTg5NTUxMjUxNDc1.NcyeQGqWmYsaKeDEQfgDw4-QrvpuvJOyH2CkRBCyHWog.5c2grQRGxA9yi79gazbM_6dv5QkKPCI0SDZfJJB7jAkg.PNG.ktojja/1.png?type=w800",
-  target: document.querySelector('.chapter-opening-quote'),
-
-});
+// don't underestimate the force
 createYoutubeEmbed({
   videoId: "L4EdtpCvF8I",
-  width: 600,
+  width: '100%',
   height: 338,
   target: document.querySelector('.chapter-opening-quote'),
   start: 0 // 시작 시간(초), 필요시 변경
 });
 
-// https://www.youtube.com/watch?v=eBddDhSsfzw
+// 관성 테니스 라켓
 createYoutubeEmbed({
   videoId: "eBddDhSsfzw",
-  width: 600,
+  width: '100%',
   height: 338,
-  target: document.querySelector('.chapter-opening-quote'),
+  target: document.querySelector('[data-translation-id="p_13"]'),
   start: 90 // 시작 시간(초), 필요시 변경
 });
 
+// 제1 법칙 예제
 createP5ExampleEmbed({
-  exampleId: "vectors/example1",
+  exampleId: "forces/example1",
   width: '100%',
   height: 680,
   base: "https://eunchae-jeon.github.io/nature-of-code-study/examples",
-  target: document.querySelector('[data-translation-id="p_8"]')
+  target: document.querySelector('[data-translation-id="p_17"]')
+});
+
+// 작용 반작용 바퀴 의자
+createImageEmbed({
+  imgUrl: "https://mblogthumb-phinf.pstatic.net/MjAyMDA1MTVfNjkg/MDAxNTg5NTUxMjUxNDc1.NcyeQGqWmYsaKeDEQfgDw4-QrvpuvJOyH2CkRBCyHWog.5c2grQRGxA9yi79gazbM_6dv5QkKPCI0SDZfJJB7jAkg.PNG.ktojja/1.png?type=w800",
+  target: document.querySelector('[data-translation-id="p_23"]'),
+  width: '100%',
 });
 
 
+// 마차
+createImageEmbed({
+  imgUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWMxR0yIjV7awYjGgYKnc1En39uJQkjpzmKg&s",
+  target: document.querySelector('[data-translation-id="p_24"]'),
+  width: '100%',
+});
+
+
+// 제3 법칙 예제
+createP5ExampleEmbed({
+  exampleId: "forces/example2",
+  width: '100%',
+  height: 680,
+  base: "https://eunchae-jeon.github.io/nature-of-code-study/examples",
+  target: document.querySelector('[data-translation-id="p_30"]')
+});
+
+// 제2 법칙 예제
+createP5ExampleEmbed({
+  exampleId: "forces/example3",
+  width: '100%',
+  height: 680,
+  base: "https://eunchae-jeon.github.io/nature-of-code-study/examples",
+  target: document.querySelector('[data-translation-id="p_35"]')
+});
